fdc3fd1398ab7888a2178c96463210255aafa22f
Marilyn Magnusen <m-mags@hotmail.co.uk>
2019-07-20 17:34:24 +0100

    fix/uniform font sizes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 export { default as IpadFrame } from './ipad-frame.png';
-export { default as profile } from './profile.jpg';
+export { default as profile } from './profile.png';
 export { default as SworkitBlog } from './sworkit-blog.png';
 export { default as SworkitHelp } from './sworkit-help.png';
 export { default as SworkitShopify } from './sworkit-shopify.png';
--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -7,16 +7,13 @@ const Introduction = () => (
     <div className='IntroductionContainer'>
         <div className='container'>
             <div className='Introduction'>
-                <section className="Introduction-title">
-                    <h2 className="section-title">Front-end Developer</h2>
-                </section>
                 <section className="Introduction-profile">
                     <img src={profile} alt="profile" className="Introduction-profileImage" />
                 </section>
             </div>
             <ScrollableAnchor id='AboutMe'>
                 <section className="AboutMe">
-                    <h2 className="section-title">About Me</h2>
+                    <h2 className="section-title">Front-end Developer</h2>
                     <p>
                     I'm a Front-end Developer who enjoys working on interesting
                         projects, of real value to the end-user. I work mostly with vanilla
